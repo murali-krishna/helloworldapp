5517a12f535ec876370d5f9d243049d682155568
Murali Krishna <murali.krishna@dubber.net>
2020-02-29 23:02:09 +1100

    added unit test

---
+++ unittest.py
@@ -0,0 +1,11 @@
+import unittest
+from mycodes import *
+
+class MyTests(unittest.TestCase):
+   ##your playground starts
+   def test_hello(self):
+       self.assertEqual(hello(), 'Hello World!')
+   ##your playground ends
+
+if __name__=="__main__":
+   unittest.main()
